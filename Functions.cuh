3d317044d6e53ebbda72c75b9ea03ba799408294
Dimitry Tegunov <tegunov@gmail.com>
2013-09-09 12:48:46 +0200

    Included type conversion function definitions.

--- Functions.cuh
+++ Functions.cuh
@@ -11,6 +11,12 @@
 	typedef cufftComplex tcomplex;
 #endif
 
+//Type conversion
+extern "C" __declspec(dllexport) tfloat* __stdcall ConvertDoubleToTFloat(double const* original, size_t const n);
+extern "C" __declspec(dllexport) double* __stdcall ConvertTFloatToDouble(float const* original, size_t const n);
+extern "C" __declspec(dllexport) tfloat* __stdcall ConvertMWComplexToTFloatInterleaved(double const* original, size_t const n);
+extern "C" __declspec(dllexport) double* __stdcall ConvertSingleInterleavedToMWComplex(tfloat const* original, size_t const n);
+
 //Fourier transform
 
 extern "C" __declspec(dllexport) void __stdcall FFT(tfloat* const d_input, tcomplex* const d_output, int const ndimensions, int3 const dimensions);
