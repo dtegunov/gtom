797a17a45579942b88049d8757b2a938f4599219
Dimitry Tegunov <tegunov@gmail.com>
2013-09-17 10:43:12 +0200

    MATLAB scripts for cumulative compilation of all MEX methods, 2 different build configs for Debug and Release (.bat files).

---
+++ MEX/compileFFT.m
@@ -0,0 +1,6 @@
+mex 'FFT\FFTR2C.cu' Helper.cu;
+mex 'FFT\FFTR2CFull.cu' Helper.cu;
+mex 'FFT\FFTC2C.cu' Helper.cu;
+mex 'FFT\IFFTC2R.cu' Helper.cu;
+mex 'FFT\IFFTC2RFull.cu' Helper.cu;
+mex 'FFT\IFFTC2C.cu' Helper.cu;
