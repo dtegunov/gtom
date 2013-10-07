f2163f17ec1e665643b712672a83fb98b0dd3d66
Dimitry Tegunov <tegunov@gmail.com>
2013-10-07 11:45:49 +0000

    generate_test_remap.m edited online with Bitbucket

--- GTOMTest/Data/FFT/generate_test_remap.m
+++ GTOMTest/Data/FFT/generate_test_remap.m
@@ -25,29 +25,3 @@ fclose(fid);
 fid = fopen('Output_Remap_2.bin','W');
 fwrite(fid,outdata,'single');
 fclose(fid);
-
-%Case 3:
-dimx=8;
-dimy=8;
-dimz=1;
-indata = reshape(ndgrid(1:(dimx*dimy*dimz)),dimx,dimy,dimz);
-outdata = fftshift(indata);
-fid = fopen('Input_Remap_3.bin','W');
-fwrite(fid,indata,'single');
-fclose(fid);
-fid = fopen('Output_Remap_3.bin','W');
-fwrite(fid,outdata,'single');
-fclose(fid);
-
-%Case 4:
-dimx=8;
-dimy=8;
-dimz=8;
-indata = reshape(ndgrid(1:(dimx*dimy*dimz)),dimx,dimy,dimz);
-outdata = fftshift(indata);
-fid = fopen('Input_Remap_4.bin','W');
-fwrite(fid,indata,'single');
-fclose(fid);
-fid = fopen('Output_Remap_4.bin','W');
-fwrite(fid,outdata,'single');
-fclose(fid);
