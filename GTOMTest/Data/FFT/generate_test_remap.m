ca3260ce952622c289c36b691bb889110d22da49
Dimitry Tegunov <tegunov@gmail.com>
2013-10-07 11:32:03 +0000

    generate_test_remap.m edited online with Bitbucket

--- GTOMTest/Data/FFT/generate_test_remap.m
+++ GTOMTest/Data/FFT/generate_test_remap.m
@@ -29,17 +29,9 @@ fclose(fid);
 %Case 3:
 dimx=8;
 dimy=8;
-<<<<<<< HEAD
-dimz=8;
-indata = reshape(ndgrid(1:(dimx*dimy*dimz)),dimx,dimy,dimz);
-outdata = ifftshift(indata);
-indata = vertcat(indata((floor(dimx/2)+1):dimx,:,:),indata(1,:,:));
-outdata = outdata(1:(floor(dimx/2)+1),:,:);
-=======
 dimz=1;
 indata = reshape(ndgrid(1:(dimx*dimy*dimz)),dimx,dimy,dimz);
 outdata = fftshift(indata);
->>>>>>> 77ee24d2625debc91b0cc36e1f8bdad326e7221b
 fid = fopen('Input_Remap_3.bin','W');
 fwrite(fid,indata,'single');
 fclose(fid);
@@ -47,23 +39,12 @@ fid = fopen('Output_Remap_3.bin','W');
 fwrite(fid,outdata,'single');
 fclose(fid);
 
-<<<<<<< HEAD
 %Case 4:
 dimx=8;
 dimy=8;
 dimz=8;
 indata = reshape(ndgrid(1:(dimx*dimy*dimz)),dimx,dimy,dimz);
 outdata = fftshift(indata);
-indata = vertcat(indata((floor(dimx/2)+1):dimx,:,:),indata(1,:,:));
-outdata = outdata(1:(floor(dimx/2)+1),:,:);
-=======
-%Case 3:
-dimx=7;
-dimy=7;
-dimz=7;
-indata = reshape(ndgrid(1:(dimx*dimy*dimz)),dimx,dimy,dimz);
-outdata = fftshift(indata);
->>>>>>> 77ee24d2625debc91b0cc36e1f8bdad326e7221b
 fid = fopen('Input_Remap_4.bin','W');
 fwrite(fid,indata,'single');
 fclose(fid);
