a6fa34a3b34bc04f37fbb8fbc203e1f6361db6f9
Dimitry Tegunov <tegunov@gmail.com>
2021-09-21 18:41:51 +0200

    Even more parameter flexibility for back-projection

--- src/Relion/Backproject.cu
+++ src/Relion/Backproject.cu
@@ -6,21 +6,21 @@
 namespace gtom
 {
 	//template<uint TpB> __global__ void Project2Dto2DKernel(cudaTex t_volumeRe, cudaTex t_volumeIm, uint dimvolume, tcomplex* d_proj, uint dimproj, uint rmax, uint rmax2);
-	template<uint ndims, bool decentered> __global__ void Backproject3DtoNDKernel(tcomplex* d_volumeft, 
-																					tfloat* d_volumeweights, 
-																					uint dimvolume, 
-																					tcomplex* d_projft, 
-																					tfloat* d_projweights, 
-																					uint dimproj, 
-																					size_t elementsproj, 
-																					glm::mat3* d_rotations, 
-																					int* d_ivolume, 
-																					glm::mat2 magnification,
-																					float ewalddiameterinv,
-																					uint rmax, 
-																					int rmax2);
+	template<uint ndims, bool decentered, bool squareinterpweights> __global__ void Backproject3DtoNDKernel(tcomplex* d_volumeft,
+		tfloat* d_volumeweights,
+		uint dimvolume,
+		tcomplex* d_projft,
+		tfloat* d_projweights,
+		uint dimproj,
+		size_t elementsproj,
+		glm::mat3* d_rotations,
+		int* d_ivolume,
+		glm::mat2 magnification,
+		float ewalddiameterinv,
+		uint rmax,
+		int rmax2);
 
-	void d_rlnBackproject(tcomplex* d_volumeft, tfloat* d_volumeweights, int3 dimsvolume, tcomplex* d_projft, tfloat* d_projweights, int3 dimsproj, uint rmax, tfloat3* h_angles, int* h_ivolume, float3 magnification, float ewaldradius, float supersample, bool outputdecentered, uint batch)
+	void d_rlnBackproject(tcomplex* d_volumeft, tfloat* d_volumeweights, int3 dimsvolume, tcomplex* d_projft, tfloat* d_projweights, int3 dimsproj, uint rmax, tfloat3* h_angles, int* h_ivolume, float4 magnification, float ewaldradius, float supersample, bool outputdecentered, bool squareinterpweights, uint batch)
 	{
 		glm::mat3* d_matrices;
 		int* d_ivolume = NULL;
@@ -36,7 +36,7 @@ namespace gtom
 				d_ivolume = (int*)CudaMallocFromHostArray(h_ivolume, sizeof(int) * batch);
 		}
 
-		d_rlnBackproject(d_volumeft, d_volumeweights, dimsvolume, d_projft, d_projweights, dimsproj, rmax, d_matrices, d_ivolume, magnification, ewaldradius * supersample, outputdecentered, batch);
+		d_rlnBackproject(d_volumeft, d_volumeweights, dimsvolume, d_projft, d_projweights, dimsproj, rmax, d_matrices, d_ivolume, magnification, ewaldradius * supersample, outputdecentered, squareinterpweights, batch);
 
 		{
 			cudaFree(d_matrices);
@@ -45,7 +45,7 @@ namespace gtom
 		}
 	}
 
-	void d_rlnBackproject(tcomplex* d_volumeft, tfloat* d_volumeweights, int3 dimsvolume, tcomplex* d_projft, tfloat* d_projweights, int3 dimsproj, uint rmax, glm::mat3* d_matrices, int* d_ivolume, float3 magnification, float ewaldradiussuper, bool outputdecentered, uint batch)
+	void d_rlnBackproject(tcomplex* d_volumeft, tfloat* d_volumeweights, int3 dimsvolume, tcomplex* d_projft, tfloat* d_projweights, int3 dimsproj, uint rmax, glm::mat3* d_matrices, int* d_ivolume, float4 magnification, float ewaldradiussuper, bool outputdecentered, bool squareinterpweights, uint batch)
 	{
 		uint ndimsvolume = DimensionCount(dimsvolume);
 		uint ndimsproj = DimensionCount(dimsproj);
@@ -53,8 +53,12 @@ namespace gtom
 			throw;
 
 		rmax = tmin(rmax, dimsproj.x / 2);
-		
-		glm::mat2 m_magnification = Matrix2Rotation(-magnification.z) * Matrix2Scale(tfloat2(magnification.x, magnification.y)) * Matrix2Rotation(magnification.z);
+
+		glm::mat2 m_magnification;
+		m_magnification[0][0] = magnification.x;
+		m_magnification[0][1] = magnification.y;
+		m_magnification[1][0] = magnification.z;
+		m_magnification[1][1] = magnification.w;
 
 		float ewalddiameterinv = ewaldradiussuper == 0 ? 0 : 1.0f / (2.0f * ewaldradiussuper);
 
@@ -63,19 +67,39 @@ namespace gtom
 			dim3 grid = dim3(1, batch, 1);
 			uint elements = ElementsFFT(dimsproj);
 
-			if (ndimsproj == 2)
+			if (squareinterpweights)
 			{
-				if (outputdecentered)
-					Backproject3DtoNDKernel<2, true> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
-				else
-					Backproject3DtoNDKernel<2, false> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+				if (ndimsproj == 2)
+				{
+					if (outputdecentered)
+						Backproject3DtoNDKernel<2, true, true> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+					else
+						Backproject3DtoNDKernel<2, false, true> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+				}
+				else if (ndimsproj == 3)
+				{
+					if (outputdecentered)
+						Backproject3DtoNDKernel<3, true, true> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+					else
+						Backproject3DtoNDKernel<3, false, true> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+				}
 			}
-			else if (ndimsproj == 3)
+			else
 			{
-				if (outputdecentered)
-					Backproject3DtoNDKernel<3, true> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
-				else
-					Backproject3DtoNDKernel<3, false> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+				if (ndimsproj == 2)
+				{
+					if (outputdecentered)
+						Backproject3DtoNDKernel<2, true, false> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+					else
+						Backproject3DtoNDKernel<2, false, false> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+				}
+				else if (ndimsproj == 3)
+				{
+					if (outputdecentered)
+						Backproject3DtoNDKernel<3, true, false> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+					else
+						Backproject3DtoNDKernel<3, false, false> << <grid, 128 >> > (d_volumeft, d_volumeweights, dimsvolume.x, d_projft, d_projweights, dimsproj.x, elements, d_matrices, d_ivolume, m_magnification, ewalddiameterinv, rmax, rmax * rmax);
+				}
 			}
 		}
 		else
@@ -99,31 +123,34 @@ namespace gtom
 		}
 	}
 
-	template<uint ndims, bool decentered> __global__ void Backproject3DtoNDKernel(tcomplex* d_volumeft, 
-																					tfloat* d_volumeweights, 
-																					uint dimvolume, 
-																					tcomplex* d_projft, 
-																					tfloat* d_projweights, 
-																					uint dimproj, 
-																					size_t elementsproj, 
-																					glm::mat3* d_rotations, 
-																					int* d_ivolume, 
-																					glm::mat2 magnification, 
-																					float ewalddiameterinv,
-																					uint rmax, 
-																					int rmax2)
+	template<uint ndims, bool decentered, bool squareinterpweights> __global__ void Backproject3DtoNDKernel(tcomplex* d_volumeft,
+																											tfloat* d_volumeweights,
+																											uint dimvolume,
+																											tcomplex* d_projft,
+																											tfloat* d_projweights,
+																											uint dimproj,
+																											size_t elementsproj,
+																											glm::mat3* d_rotations,
+																											int* d_ivolume,
+																											glm::mat2 magnification,
+																											float ewalddiameterinv,
+																											uint rmax,
+																											int rmax2)
 	{
-		d_projft += elementsproj * blockIdx.y;
-		d_projweights += elementsproj * blockIdx.y;
+		if (d_projft != NULL)
+			d_projft += elementsproj * blockIdx.y;
+		if (d_projweights != NULL)
+			d_projweights += elementsproj * blockIdx.y;
 
 		if (d_ivolume != NULL)
 		{
 			int ivolume = d_ivolume[blockIdx.y];
-			d_volumeft += ElementsFFT1(dimvolume) * dimvolume * dimvolume * ivolume;
+			if (d_volumeft != NULL)
+				d_volumeft += ElementsFFT1(dimvolume) * dimvolume * dimvolume * ivolume;
 			if (d_volumeweights != NULL)
 				d_volumeweights += ElementsFFT1(dimvolume) * dimvolume * dimvolume * ivolume;
 		}
-		
+
 		uint slice = ndims == 3 ? ElementsFFT1(dimproj) * dimproj : 1;
 		uint dimft = ElementsFFT1(dimproj);
 		uint dimvolumeft = ElementsFFT1(dimvolume);
@@ -140,6 +167,17 @@ namespace gtom
 			int y = idy <= dimproj / 2 ? idy : (int)idy - (int)dimproj;
 			int z = ndims == 3 ? (idz <= dimproj / 2 ? idz : (int)idz - (int)dimproj) : 0;
 
+			if (ndims == 3)
+			{
+				if (x == 0 && y < 0 && z < 0)
+					continue;
+			}
+			else
+			{
+				if (x == 0 && y < 0)
+					continue;
+			}
+
 			glm::vec2 posmag = glm::vec2(x, y);
 			if (ndims == 2)
 				posmag = magnification * posmag;
@@ -147,117 +185,138 @@ namespace gtom
 			glm::vec3 pos = glm::vec3(posmag.x, posmag.y, z);
 			if (ndims == 2)
 				pos.z = ewalddiameterinv * (x * x + y * y);
-			
+
 			int r2 = ndims == 3 ? (z * z + y * y + x * x) : (pos.x * pos.x + pos.y * pos.y + pos.z * pos.z);
 
 			if (r2 >= rmax2)
 				continue;
-			
+
 			pos = rotation * pos;
 
-			// Only asymmetric half is stored
-			float is_neg_x = 1.0f;
-			if (pos.x + 1e-5f < 0)
-			{
-				// Get complex conjugated hermitian symmetry pair
-				pos.x = abs(pos.x);
-				pos.y = -pos.y;
-				pos.z = -pos.z;
-				is_neg_x = -1.0f;
-			}
-
 			// Trilinear interpolation
-			int x0 = floor(pos.x + 1e-5f);
+			short x0 = (short)floor(pos.x + 1e-5f);
 			pos.x -= x0;
-			int x1 = x0 + 1;
-			x0 = tmin(dimvolume / 2, x0);
-			x1 = tmin(dimvolume / 2, x1);
+			short x1 = x0 + 1;
 
-			int y0 = floor(pos.y);
+			short y0 = (short)floor(pos.y);
 			pos.y -= y0;
-			y0 += dimvolume / 2;
-			int y1 = y0 + 1;
-			y0 = tmin(dimvolume - 1, y0);
-			y1 = tmin(dimvolume - 1, y1);
+			short y1 = y0 + 1;
 
-			int z0 = floor(pos.z);
+			short z0 = (short)floor(pos.z);
 			pos.z -= z0;
-			z0 += dimvolume / 2;
-			int z1 = z0 + 1;
-			z0 = tmin(dimvolume - 1, z0);
-			z1 = tmin(dimvolume - 1, z1);
+			short z1 = z0 + 1;
+
+			short3 positions[8];
+
+			positions[0] = make_short3(x0, y0, z0);
+			positions[1] = make_short3(x1, y0, z0);
+			positions[2] = make_short3(x0, y1, z0);
+			positions[3] = make_short3(x1, y1, z0);
+
+			positions[4] = make_short3(x0, y0, z1);
+			positions[5] = make_short3(x1, y0, z1);
+			positions[6] = make_short3(x0, y1, z1);
+			positions[7] = make_short3(x1, y1, z1);
 
 			float c0 = 1.0f - pos.z;
 			float c1 = pos.z;
 
-			float c00 = (1.0f - pos.y) * c0;
-			float c10 = pos.y * c0;
-			float c01 = (1.0f - pos.y) * c1;
-			float c11 = pos.y * c1;
+			half c00 = __float2half((1.0f - pos.y) * c0);
+			half c10 = __float2half(pos.y * c0);
+			half c01 = __float2half((1.0f - pos.y) * c1);
+			half c11 = __float2half(pos.y * c1);
 
-			float c000 = (1.0f - pos.x) * c00;
-			float c100 = pos.x * c00;
-			float c010 = (1.0f - pos.x) * c10;
-			float c110 = pos.x * c10;
-			float c001 = (1.0f - pos.x) * c01;
-			float c101 = pos.x * c01;
-			float c011 = (1.0f - pos.x) * c11;
-			float c111 = pos.x * c11;
+			half interpw[8];
+			interpw[0] = __float2half((1.0f - pos.x) * __half2float(c00));
+			interpw[1] = __float2half(pos.x * __half2float(c00));
+			interpw[2] = __float2half((1.0f - pos.x) * __half2float(c10));
+			interpw[3] = __float2half(pos.x * __half2float(c10));
 
-			tcomplex val = d_projft[id];
-			val.y *= is_neg_x;
+			interpw[4] = __float2half((1.0f - pos.x) * __half2float(c01));
+			interpw[5] = __float2half(pos.x * __half2float(c01));
+			interpw[6] = __float2half((1.0f - pos.x) * __half2float(c11));
+			interpw[7] = __float2half(pos.x * __half2float(c11));
 
-			if (decentered)
+			tcomplex val = make_float2(1, 0);
+			if (d_projft != NULL)
+				val = d_projft[id];
+
+			tfloat weight = 1;
+			if (d_projweights != NULL)
+				weight = d_projweights[id];
+
+			for (uint i = 0; i < 8; i++)
 			{
-				/*z0 = z0 < dimvolume / 2 ? z0 + dimvolume / 2 : z0 - dimvolume / 2;
-				z1 = z1 < dimvolume / 2 ? z1 + dimvolume / 2 : z1 - dimvolume / 2;
+				tcomplex valsym = val;
+				short3 position = positions[i];
 
-				y0 = y0 < dimvolume / 2 ? y0 + dimvolume / 2 : y0 - dimvolume / 2;
-				y1 = y1 < dimvolume / 2 ? y1 + dimvolume / 2 : y1 - dimvolume / 2;*/
+				if (positions[i].x < 0)
+				{
+					position.x *= -1;
+					position.y *= -1;
+					position.z *= -1;
+					valsym.y *= -1;
+				}
 
-				z0 = (z0 + dimvolume / 2) % dimvolume;
-				z1 = (z1 + dimvolume / 2) % dimvolume;
+				if (decentered)
+				{
+					position.y = position.y < 0 ? position.y + dimvolume : position.y;
+					position.z = position.z < 0 ? position.z + dimvolume : position.z;
+				}
+				else
+				{
+					position.y += dimvolume / 2;
+					position.z += dimvolume / 2;
+				}
 
-				y0 = (y0 + dimvolume / 2) % dimvolume;
-				y1 = (y1 + dimvolume / 2) % dimvolume;
-			}
+				position.x = tmin(dimvolume / 2, position.x);
+				position.y = tmax(0, tmin(dimvolume - 1, position.y));
+				position.z = tmax(0, tmin(dimvolume - 1, position.z));
 
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y0) * dimvolumeft + x0), c000 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y0) * dimvolumeft + x0) + 1, c000 * val.y);
+				float interpweight = __half2float(interpw[i]);
+				if (squareinterpweights)
+					interpweight *= interpweight;
 
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y0) * dimvolumeft + x1), c100 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y0) * dimvolumeft + x1) + 1, c100 * val.y);
+				if (d_volumeft != NULL)
+				{
+					atomicAdd((tfloat*)(d_volumeft + (position.z * dimvolume + position.y) * dimvolumeft + position.x), interpweight * valsym.x);
+					atomicAdd((tfloat*)(d_volumeft + (position.z * dimvolume + position.y) * dimvolumeft + position.x) + 1, interpweight * valsym.y);
+				}
 
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y1) * dimvolumeft + x0), c010 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y1) * dimvolumeft + x0) + 1, c010 * val.y);
+				if (d_volumeweights != NULL)
+					atomicAdd((tfloat*)(d_volumeweights + (position.z * dimvolume + position.y) * dimvolumeft + position.x), interpweight * weight);
 
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y1) * dimvolumeft + x1), c110 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z0 * dimvolume + y1) * dimvolumeft + x1) + 1, c110 * val.y);
+				if (positions[i].x == 0 && (positions[i].y != 0 || positions[i].z != 0))
+				{
+					position = positions[i];
+					position.x *= -1;
+					position.y *= -1;
+					position.z *= -1;
 
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y0) * dimvolumeft + x0), c001 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y0) * dimvolumeft + x0) + 1, c001 * val.y);
+					if (decentered)
+					{
+						position.y = position.y < 0 ? position.y + dimvolume : position.y;
+						position.z = position.z < 0 ? position.z + dimvolume : position.z;
+					}
+					else
+					{
+						position.y += dimvolume / 2;
+						position.z += dimvolume / 2;
+					}
 
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y0) * dimvolumeft + x1), c101 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y0) * dimvolumeft + x1) + 1, c101 * val.y);
+					position.x = tmin(dimvolume / 2, position.x);
+					position.y = tmax(0, tmin(dimvolume - 1, position.y));
+					position.z = tmax(0, tmin(dimvolume - 1, position.z));
 
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y1) * dimvolumeft + x0), c011 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y1) * dimvolumeft + x0) + 1, c011 * val.y);
+					if (d_volumeft != NULL)
+					{
+						atomicAdd((tfloat*)(d_volumeft + (position.z * dimvolume + position.y) * dimvolumeft + position.x), interpweight * valsym.x);
+						atomicAdd((tfloat*)(d_volumeft + (position.z * dimvolume + position.y) * dimvolumeft + position.x) + 1, interpweight * (-valsym.y));
+					}
 
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y1) * dimvolumeft + x1), c111 * val.x);
-			atomicAdd((tfloat*)(d_volumeft + (z1 * dimvolume + y1) * dimvolumeft + x1) + 1, c111 * val.y);
-
-			if (d_volumeweights != NULL)
-			{
-				tfloat weight = d_projweights[id];
-
-				atomicAdd((tfloat*)(d_volumeweights + (z0 * dimvolume + y0) * dimvolumeft + x0), c000 * weight);
-				atomicAdd((tfloat*)(d_volumeweights + (z0 * dimvolume + y0) * dimvolumeft + x1), c100 * weight);
-				atomicAdd((tfloat*)(d_volumeweights + (z0 * dimvolume + y1) * dimvolumeft + x0), c010 * weight);
-				atomicAdd((tfloat*)(d_volumeweights + (z0 * dimvolume + y1) * dimvolumeft + x1), c110 * weight);
-				atomicAdd((tfloat*)(d_volumeweights + (z1 * dimvolume + y0) * dimvolumeft + x0), c001 * weight);
-				atomicAdd((tfloat*)(d_volumeweights + (z1 * dimvolume + y0) * dimvolumeft + x1), c101 * weight);
-				atomicAdd((tfloat*)(d_volumeweights + (z1 * dimvolume + y1) * dimvolumeft + x0), c011 * weight);
-				atomicAdd((tfloat*)(d_volumeweights + (z1 * dimvolume + y1) * dimvolumeft + x1), c111 * weight);
+					if (d_volumeweights != NULL)
+						atomicAdd((tfloat*)(d_volumeweights + (position.z * dimvolume + position.y) * dimvolumeft + position.x), interpweight * weight);
+				}
 			}
 		}
 	}
